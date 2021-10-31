448c98dcec66d63d35318eb0191a038f77a0a381
Weston Vincze <westonvincze@gmail.com>
2021-10-31 17:34:19 -0400

    Adds cinemachine and animation events base class

---
+++ Assets/Scripts/Character/Animation/AnimationEvents.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts.Character.Animation
+{
+    class AnimationEvents : MonoBehaviour
+    {
+    }
+}
